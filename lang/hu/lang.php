02b334c27663217b2809675ee38388fdbb7431dd
Szabó Gergő <gergo85@gmail.com>
2017-10-27 00:59:33 +0200

    Add the Hungarian translate (#37)

---
+++ lang/hu/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Szerkesztő',
+        'description' => 'Megjelenő tartalmak gyors szerkesztése.',
+    ],
+    'component_editable' => [
+        'name' => 'Szerkesztő',
+        'description' => 'Megjelenő tartalmak gyors szerkesztése.',
+        'property_file' => [
+            'title' => 'Fájl',
+            'description' => 'A szerkeszteni kívánt tartalom (nem kötelező megadni).',
+        ],
+    ],
+];
