f49b2f3f14142e141b196380771ad9bd7c073029
Mahmut Namli <mahmudnamli@gmail.com>
2019-02-07 17:26:41 +0300

    Create tr/lang.php
    Strings from en/lang.php copied and also translated into Turkish language

---
+++ lang/tr/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Editable',
+        'description' => 'Doğrudan içerik editörü',
+    ],
+    'component_editable' => [
+        'name' => 'Editable Bileşeni',
+        'description' => 'Bu bileşen, içerik içi düzenlemeye izin verir.',
+        'property_file' => [
+            'title' => 'Dosya',
+            'description' => 'Düzenlenecek içerik bloğu dosya adı, isteğe bağlı',
+        ],
+    ],
+];
