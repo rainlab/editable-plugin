e5f49da6f609eef009ab9040522f1e28f5a53769
Blaž Oražem <blaz@orazem.si>
2019-12-03 17:00:00 +0100

    Added Slovenian translation (#43)
    Credit to @BlazOrazem

---
+++ lang/sl/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Neposredni urejevalnik',
+        'description' => 'Urejevalnik vsebine v kontekstu na spletni strani.',
+    ],
+    'component_editable' => [
+        'name' => 'Neposredni urejevalnik vsebine',
+        'description' => 'Ta komponenta omogoča urejanje vsebine neposredno na spletni strani.',
+        'property_file' => [
+            'title' => 'Datoteka',
+            'description' => 'Ime datoteke bloka vsebine za urejanje, neobvezno.',
+        ],
+    ],
+];
